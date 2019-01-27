030fd84e119b2443025421e6e9393e6bc85b3ed6
Luke Bennett <luke.bennett88@gmail.com>
2019-01-27 23:51:37 +1100

    Fix contact form

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,112 +1,30 @@
 import React from 'react';
 import Image from '../images/contact.png';
+import Form from './form';
 
 const Contact = () => (
   <section id="contact" className="background bg-pink px-4 py-16 text-white">
-    <div className="flex flex-wrap items-start max-w-2xl mx-auto">
-      <div className="p-4 w-full md:w-1/2">
+    <div className="flex flex-wrap max-w-2xl mx-auto">
+      <div className="flex flex-col flex-wrap items-start leading-normal p-4 md:w-1/2">
         <h2 className="font-condensed mb-4 text-4xl uppercase">Contact</h2>
-        <p className="flex font-bold mb-3">
+        <p className="font-bold">
           <span className="mr-2">Phone: </span>
           <a className="text-white" href="tel:0404222244">
             0404 222 244
           </a>
         </p>
-        <p className="flex font-bold md:mb-12 md:pb-2">
+        <p className="flex font-bold">
           <span className="mr-2">Email: </span>
           <a className="text-white" href="mailto:pinkslipproperty@icloud.com">
             pinkslipproperty@icloud.com
           </a>
         </p>
-        <p className="hidden md:block">
+        <p className="hidden md:block md:self-end md:mb-16 mt-auto pb-1">
           <img src={Image} alt="" />
         </p>
       </div>
 
-      <form
-        name="contact-form"
-        action="/success"
-        method="post"
-        className="p-4 w-full md:w-1/2"
-        autoComplete="on"
-        data-netlify="true"
-        data-netlify-honeypot="bot-field"
-      >
-        <div className="flex flex-wrap mb-6 -mx-3">
-          <div className="mb-6 px-3 w-full md:mb-0 md:w-1/2">
-            <input
-              type="text"
-              className="appearance-none bg-grey-lighter block border leading-tight px-4 py-3 rounded-none text-grey-darker w-full focus:bg-white focus:border-indigo focus:outline-none hover:bg-white"
-              id="fname"
-              name="fname"
-              placeholder="First name"
-              autoComplete="given-name"
-              required
-            />
-          </div>
-          <div className="mb-6 px-3 w-full md:mb-0 md:w-1/2">
-            <input
-              type="text"
-              className="appearance-none bg-grey-lighter block border leading-tight px-4 py-3 rounded-none text-grey-darker w-full focus:bg-white focus:border-indigo focus:outline-none hover:bg-white"
-              id="lname"
-              name="lname"
-              placeholder="Last name"
-              autoComplete="family-name"
-              required
-            />
-          </div>
-        </div>
-        <div className="flex flex-wrap -mx-3 mb-6">
-          <div className="mb-6 px-3 w-full md:mb-0 md:w-1/2">
-            <input
-              type="text"
-              className="appearance-none bg-grey-lighter block border leading-tight px-4 py-3 rounded-none text-grey-darker w-full focus:bg-white focus:border-indigo focus:outline-none hover:bg-white"
-              id="phone"
-              name="phone"
-              placeholder="Phone number"
-              autoComplete="tel-national"
-              required
-            />
-          </div>
-          <div className="mb-6 px-3 w-full md:mb-0 md:w-1/2">
-            <input
-              type="text"
-              className="appearance-none bg-grey-lighter block border leading-tight px-4 py-3 rounded-none text-grey-darker w-full focus:bg-white focus:border-indigo focus:outline-none hover:bg-white"
-              id="email"
-              name="email"
-              placeholder="Email address"
-              autoComplete="email"
-              required
-            />
-          </div>
-        </div>
-        <div className="flex flex-wrap mb-12 md:mb-6">
-          <textarea
-            className="appearance-none bg-grey-lighter block border h-48 leading-tight px-4 py-3 rounded-none text-grey-darker w-full focus:bg-white focus:border-indigo focus:outline-none hover:bg-white"
-            id="message"
-            name="message"
-            placeholder="Message"
-            spellCheck="true"
-            required
-            defaultValue={''}
-          />
-        </div>
-        <div class="hidden">
-          <input type="hidden" name="form-name" value="contact" />
-          <label>Don’t fill this out if you're human:</label>
-          <input name="bot-field" />
-        </div>
-        <div className="flex items-center justify-end">
-          <button
-            className="bg-indigo-darker font-bold font-condensed inline-block px-12 py-4 no-underline text-white uppercase"
-            id="submit"
-            name="submit"
-            type="submit"
-          >
-            Send
-          </button>
-        </div>
-      </form>
+      <Form />
     </div>
   </section>
 );
---
+++ src/components/form.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { navigateTo } from 'gatsby-link';
+
+function encode(data) {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&');
+}
+
+export default class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const form = e.target;
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({
+        'form-name': form.getAttribute('name'),
+        ...this.state,
+      }),
+    })
+      .then(() => navigateTo(form.getAttribute('action')))
+      .catch(error => alert(error));
+  };
+
+  render() {
+    return (
+      <form
+      className="p-4 w-full md:w-1/2"
+        name="contact"
+        method="post"
+        action="/success/"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+        onSubmit={this.handleSubmit}
+      >
+        {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+        <input type="hidden" name="form-name" value="contact" />
+        <p hidden>
+          <label>
+            Don’t fill this out:{' '}
+            <input name="bot-field" onChange={this.handleChange} />
+          </label>
+        </p>
+        <p>
+          <label className="block font-bold mb-6 text-xs tracking-wide uppercase">
+            <span className="block mb-2">Your name:</span>
+            <input className="appearance-none bg-grey-lighter block border leading-tight px-4 py-3 rounded-none text-base w-full focus:bg-white focus:border-indigo focus:outline-none hover:bg-white" type="text" name="name" onChange={this.handleChange} />
+          </label>
+        </p>
+        <p>
+          <label className="block font-bold mb-6 text-xs tracking-wide uppercase">
+            <span className="block mb-2">Your email:</span>
+            <input className="appearance-none bg-grey-lighter block border leading-tight px-4 py-3 rounded-none text-base w-full focus:bg-white focus:border-indigo focus:outline-none hover:bg-white" type="email" name="email" onChange={this.handleChange} />
+          </label>
+        </p>
+        <p>
+          <label className="block font-bold mb-6 text-xs tracking-wide uppercase">
+            <span className="block mb-2">Message:</span>
+            <textarea className="appearance-none bg-grey-lighter block border h-48 leading-tight px-4 py-3 rounded-none text-grey-darker w-full focus:bg-white focus:border-indigo focus:outline-none hover:bg-white" name="message" onChange={this.handleChange} />
+          </label>
+        </p>
+        <p className="flex justify-end">
+          <button className="bg-indigo-darker font-bold font-condensed inline-block px-12 py-4 no-underline shadow text-white uppercase" type="submit">Send</button>
+        </p>
+      </form>
+    );
+  }
+}
