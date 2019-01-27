41a939185fa8c715dc864fe5a2bcc2add32267d4
Luke Bennett <luke.bennett88@gmail.com>
2019-01-28 00:09:43 +1100

    Update copy for success page

--- src/pages/success.js
+++ src/pages/success.js
@@ -4,13 +4,16 @@ import Layout from '../components/layout';
 
 const Success = () => (
   <Layout>
-    <div className="flex flex-col items-center justify-center leading-normal py-16 text-center">
+    <div className="flex flex-col items-center justify-center leading-loose py-16">
       <Link className="no-underline" to="/">
         <h1 className="font-condensed text-grey-darkest text-5xl uppercase">
-          Message received!
+          Message sent
         </h1>
+        <p className="text-grey-darkest">Thank you for contacting Pinkslip Property.</p>
+        <p className="text-grey-darkest">We will get back to you as soon as possible.</p>
+        <p className="text-grey-darkest">Please call <a className="font-bold no-underline text-pink text-lg" href="tel:0404222244">0404 222 244</a> if you matter is urgent.</p>
         <p className="font-bold no-underline text-pink text-lg">
-          Click to return to the home page
+          Click here to return to the homepage.
         </p>
       </Link>
     </div>
