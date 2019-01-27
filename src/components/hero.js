6df6c1e85b9c9aea3abf2a8f279b14f66e9522fb
Luke Bennett <luke.bennett88@gmail.com>
2019-01-27 23:55:50 +1100

    Use new icon

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import Check from '../images/check.svg';
+import Check from '../images/check-pink.svg';
 import '../css/beach.css';
 
 const Hero = () => (
-  <section className="beach bg-no-repeat bg-top bg-pink bg-cover flex flex-col font-condensed h-full justify-center py-8 md:py-32 text-center w-full">
+  <section className="beach bg-fixed bg-no-repeat bg-top bg-pink bg-cover flex flex-col font-condensed h-full justify-center py-8 md:py-32 text-center w-full">
     <div className="font-bold leading-normal max-w-md mb-8 md:mb-16 mx-auto px-4 uppercase text-white text-3xl md:text-4xl w-full">
       <p>Property pink slips just $199</p>
       <p>
