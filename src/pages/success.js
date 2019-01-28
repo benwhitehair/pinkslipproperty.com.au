78fb6eddafc24e4da81584c12d34cb208ede3005
Luke Bennett <luke.bennett88@gmail.com>
2019-01-28 20:49:18 +1100

    Formatting

--- src/pages/success.js
+++ src/pages/success.js
@@ -9,9 +9,22 @@ const Success = () => (
         <h1 className="font-condensed text-grey-darkest text-5xl uppercase">
           Message sent
         </h1>
-        <p className="text-grey-darkest">Thank you for contacting Pinkslip Property.</p>
-        <p className="text-grey-darkest">We will get back to you as soon as possible.</p>
-        <p className="text-grey-darkest">Please call <a className="font-bold no-underline text-pink text-lg" href="tel:0404222244">0404 222 244</a> if you matter is urgent.</p>
+        <p className="text-grey-darkest">
+          Thank you for contacting Pinkslip Property.
+        </p>
+        <p className="text-grey-darkest">
+          We will get back to you as soon as possible.
+        </p>
+        <p className="text-grey-darkest">
+          Please call{' '}
+          <a
+            className="font-bold no-underline text-pink text-lg"
+            href="tel:0404222244"
+          >
+            0404 222 244
+          </a>{' '}
+          if you matter is urgent.
+        </p>
         <p className="font-bold no-underline text-pink text-lg">
           Click here to return to the homepage.
         </p>
