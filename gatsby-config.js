6e46eea2e340fe22522d3bc6d06ae74312d927d8
Luke Bennett <luke.bennett88@gmail.com>
2019-01-28 13:07:01 +1100

    Use realfavicongenerator.com for icons

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,28 +15,13 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-tailwind`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#f17bac`,
-        display: `minimal-ui`,
-        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
-      },
-    },
     `gatsby-plugin-postcss`,
     {
-      resolve: 'gatsby-plugin-purgecss',
+      resolve: `gatsby-plugin-purgecss`,
       options: {
         tailwind: true,
-        purgeOnly: ['src/css/style.css'], // Purge only tailwind
+        purgeOnly: [`src/css/style.css`], // Purge only tailwind
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
   ],
 };
