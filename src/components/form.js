7be8556dca48463007891c4208f46de98fc86f9e
Luke Bennett <luke.bennett88@gmail.com>
2019-01-28 11:19:13 +1100

    Formatting

--- src/components/form.js
+++ src/components/form.js
@@ -35,7 +35,7 @@ export default class Contact extends React.Component {
   render() {
     return (
       <form
-      className="p-4 w-full md:w-1/2"
+        className="p-4 w-full md:w-1/2"
         name="contact"
         method="post"
         action="/success/"
@@ -54,29 +54,56 @@ export default class Contact extends React.Component {
         <p>
           <label className="block font-bold mb-6 text-xs tracking-wide uppercase">
             <span className="block mb-2">Full name:</span>
-            <input className="appearance-none bg-grey-lighter block border leading-tight px-4 py-3 rounded-none text-base w-full focus:bg-white focus:border-indigo focus:outline-none hover:bg-white" type="text" name="name" required onChange={this.handleChange} />
+            <input
+              className="appearance-none bg-grey-lighter block border leading-tight px-4 py-3 rounded-none text-base w-full focus:bg-white focus:border-indigo focus:outline-none hover:bg-white"
+              type="text"
+              name="name"
+              required
+              onChange={this.handleChange}
+            />
           </label>
         </p>
         <p>
           <label className="block font-bold mb-6 text-xs tracking-wide uppercase">
             <span className="block mb-2">Phone number:</span>
-            <input className="appearance-none bg-grey-lighter block border leading-tight px-4 py-3 rounded-none text-base w-full focus:bg-white focus:border-indigo focus:outline-none hover:bg-white" type="text" name="phone" required onChange={this.handleChange} />
+            <input
+              className="appearance-none bg-grey-lighter block border leading-tight px-4 py-3 rounded-none text-base w-full focus:bg-white focus:border-indigo focus:outline-none hover:bg-white"
+              type="text"
+              name="phone"
+              required
+              onChange={this.handleChange}
+            />
           </label>
         </p>
         <p>
           <label className="block font-bold mb-6 text-xs tracking-wide uppercase">
-            <span className="block mb-2">Your email:</span>
-            <input className="appearance-none bg-grey-lighter block border leading-tight px-4 py-3 rounded-none text-base w-full focus:bg-white focus:border-indigo focus:outline-none hover:bg-white" type="email" name="email" onChange={this.handleChange} />
+            <span className="block mb-2">Email address:</span>
+            <input
+              className="appearance-none bg-grey-lighter block border leading-tight px-4 py-3 rounded-none text-base w-full focus:bg-white focus:border-indigo focus:outline-none hover:bg-white"
+              type="email"
+              name="email"
+              onChange={this.handleChange}
+            />
           </label>
         </p>
         <p>
           <label className="block font-bold mb-6 text-xs tracking-wide uppercase">
             <span className="block mb-2">Message:</span>
-            <textarea className="appearance-none bg-grey-lighter block border h-48 leading-tight px-4 py-3 rounded-none text-base text-grey-darker w-full focus:bg-white focus:border-indigo focus:outline-none hover:bg-white" name="message" required onChange={this.handleChange} />
+            <textarea
+              className="appearance-none bg-grey-lighter block border h-48 leading-tight px-4 py-3 rounded-none text-base text-grey-darker w-full focus:bg-white focus:border-indigo focus:outline-none hover:bg-white"
+              name="message"
+              required
+              onChange={this.handleChange}
+            />
           </label>
         </p>
         <p className="flex justify-end">
-          <button className="bg-indigo-darker font-bold font-condensed inline-block px-12 py-4 no-underline shadow text-white uppercase" type="submit">Send</button>
+          <button
+            className="bg-indigo-darker font-bold font-condensed inline-block px-12 py-4 no-underline shadow text-white uppercase"
+            type="submit"
+          >
+            Send
+          </button>
         </p>
       </form>
     );
