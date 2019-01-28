e0e0a86cf7d8167326da4235fca1be7b113d57e6
Luke Bennett <luke.bennett88@gmail.com>
2019-01-28 11:18:15 +1100

    Style contact info and add CSS for background image

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import Image from '../images/contact.png';
 import Form from './form';
+import '../css/background.css';
 
 const Contact = () => (
-  <section id="contact" className="background bg-pink px-4 py-16 text-white">
+  <section
+    id="contact"
+    className="background bg-pink px-4 py-16 text-indigo-darkest"
+  >
     <div className="flex flex-wrap max-w-2xl mx-auto">
       <div className="flex flex-col flex-wrap items-start leading-normal p-4 md:w-1/2">
         <h2 className="font-condensed mb-4 text-4xl uppercase">Contact</h2>
-        <p className="font-bold">
+        <p className="font-bold text-lg">
           <span className="mr-2">Phone: </span>
           <a className="text-white" href="tel:0404222244">
             0404 222 244
           </a>
         </p>
-        <p className="flex font-bold">
+        <p className="font-bold text-lg">
           <span className="mr-2">Email: </span>
           <a className="text-white" href="mailto:pinkslipproperty@icloud.com">
             pinkslipproperty@icloud.com
