5217dd85fbcea5e55dcc60cc662647ff647485af
Luke Bennett <luke.bennett88@gmail.com>
2019-01-28 11:18:24 +1100

    Increase text size

--- src/components/cta2.js
+++ src/components/cta2.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../css/background.css'
+import '../css/background.css';
 
 const CTA2 = () => (
   <section className="background bg-pink px-4 py-16 text-center text-white">
@@ -7,7 +7,7 @@ const CTA2 = () => (
       Combine all our checks for the all inclusive total of{' '}
       <span className="text-5xl">$199</span>
     </h2>
-    <p className="text-xl">
+    <p className="text-2xl">
       Also includes 2 x alarms (if required) featuring a 5 year warranty and 10
       year certificate
     </p>
