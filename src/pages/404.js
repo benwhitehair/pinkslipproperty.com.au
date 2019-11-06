e6baa8ef03731a962043e74a3165b7204cee86ee
Luke Bennett <hello@lukebennett.com.au>
2019-11-07 09:41:40 +1100

    Move links in

--- src/pages/404.js
+++ src/pages/404.js
@@ -8,14 +8,14 @@ const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
     <div className="flex flex-col items-center justify-center leading-normal py-16 text-center">
-      <Link className="no-underline" to="/">
-        <h1 className="font-condensed text-grey-darkest text-5xl uppercase">
-          404: Page not found
-        </h1>
-        <p className="font-bold no-underline text-pink-dark text-lg">
+      <h1 className="font-condensed text-grey-darkest text-5xl uppercase">
+        404: Page not found
+      </h1>
+      <p>
+        <Link to="/" className="font-bold no-underline text-pink-dark text-lg">
           Click to return to the home page
-        </p>
-      </Link>
+        </Link>
+      </p>
     </div>
   </Layout>
 );
