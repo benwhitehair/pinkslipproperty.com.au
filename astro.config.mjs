a63740e53ea6d56deb5c191fe6dea301f8add18f
Luke Bennett <hello@lukebennett.com.au>
2024-07-19 16:39:41 +1000

    Update and SEO fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,21 @@
-import markdoc from '@astrojs/markdoc';
 import netlify from '@astrojs/netlify';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
+
 import { siteConfig } from './src/lib/site-config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: siteConfig.baseUrl,
+	adapter: netlify({
+		edgeMiddleware: true,
+		imageCDN: true,
+	}),
+	integrations: [sitemap(), tailwind()],
+	output: 'hybrid',
 	server: {
 		host: true,
 	},
-	integrations: [markdoc(), sitemap(), tailwind()],
-	output: 'hybrid',
-	adapter: netlify({
-		edgeMiddleware: true,
-	}),
+	site: siteConfig.baseUrl,
+	trailingSlash: 'never',
 });
